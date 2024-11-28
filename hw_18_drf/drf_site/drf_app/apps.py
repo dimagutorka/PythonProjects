6c7d305167e487efa09c8861b21dceb6f40022b9
dimagutorka <dgutorka@gmail.com>
2024-11-28 22:05:11 +0200

    Homework 20

--- hw_18_drf/drf_site/drf_app/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class DrfAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'drf_app'
-
-    def ready(self):
-        from drf_app import signals
