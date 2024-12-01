3aad8b665e75d6211fed6f48d0c4ba055907a94b
dimagutorka <dgutorka@gmail.com>
2024-12-01 16:18:27 +0200

    .

---
+++ hw_16_models_orm/my_project_board/board_app/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+
+class BoardAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'board_app'
+
+
+    def ready(self):
+        import board_app.signals
