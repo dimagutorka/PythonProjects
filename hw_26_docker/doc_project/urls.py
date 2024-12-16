141e4fa21ed6b21ffd4d69c3c1b3b6c7dd3b87ae
dimagutorka <dgutorka@gmail.com>
2024-12-16 14:53:08 +0200

    Finished HW 26 with docker

--- hw_26_docker/doc_project/urls.py
+++ hw_26_docker/doc_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for core project.
+URL configuration for doc_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from docker_app.views import index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', index, name='index'),
 ]
